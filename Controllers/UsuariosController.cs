cd5bc5a077076b68d2558ba000c899edec4ed804
Francis Emanuel Castillo Vasquez <francis_castillo4@ucne.edu.do>
2025-11-18 00:10:40 -0400

    Agregar archivos de proyecto.

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using LiteraVerseApi.DAL;
+using LiteraVerseApi.Models;
+
+namespace LiteraVerseApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuariosController : ControllerBase
+    {
+        private readonly Contexto _context;
+
+        public UsuariosController(Contexto context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Usuarios>>> GetUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Usuarios>> GetUsuarios(int id)
+        {
+            var usuarios = await _context.Usuarios.FindAsync(id);
+
+            if (usuarios == null)
+            {
+                return NotFound();
+            }
+
+            return usuarios;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUsuarios(int id, Usuarios usuarios)
+        {
+            if (id != usuarios.UsuarioId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(usuarios).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UsuariosExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Usuarios>> PostUsuarios(Usuarios usuarios)
+        {
+            _context.Usuarios.Add(usuarios);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetUsuarios", new { id = usuarios.UsuarioId }, usuarios);
+        }
+
+        private bool UsuariosExists(int id)
+        {
+            return _context.Usuarios.Any(e => e.UsuarioId == id);
+        }
+    }
+}
---
+++ DAL/Contexto.cs
@@ -0,0 +1,9 @@
+﻿using LiteraVerseApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace LiteraVerseApi.DAL;
+
+public class Contexto(DbContextOptions<Contexto> options) : DbContext(options)
+{
+    public DbSet<Usuarios> Usuarios { get; set; }
+}
