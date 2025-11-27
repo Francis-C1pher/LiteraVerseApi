dfd0d32fd06521e4b65ed6b123e34fc99cda74ab
MarianVentura <marianv1119@gmail.com>
2025-11-27 14:20:35 -0400

    Db updates

--- Migrations/20251127181918_Inicial.Designer.cs
+++ Migrations/20251127181918_Inicial.Designer.cs
@@ -11,8 +11,8 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LiteraVerseApi.Migrations
 {
     [DbContext(typeof(Contexto))]
-    [Migration("20251126040441_InitialCreate")]
-    partial class InitialCreate
+    [Migration("20251127181918_Inicial")]
+    partial class Inicial
     {
         /// <inheritdoc />
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
